db804f965b2d299f35acbee001b015546e21b148
Ani1411 <42236106+Ani1411@users.noreply.github.com>
2018-10-03 20:35:58 +0530

    Update Feed.js

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -4,11 +4,29 @@ import "./../css/feed.css"
 export default class Feed extends Component{
     render(){
         return(
+           <div className="row">
             <div className="feedBox">
                 <h3>Name of uploader</h3>
-                <p>Caption of uploader</p>
-                <img src=""/>
-                         </div>
+                <p>Caption </p>
+                <img  className='image' src=""/>
+            </div>
+
+            <div className="feedBox">
+            <h3>Name of uploader</h3>
+        <p>Caption </p>
+        <img src=""/>
+            </div>
+        <div className="feedBox">
+            <h3>Name of uploader</h3>
+            <p>Caption </p>
+            <img src=""/>
+        </div>
+        <div className="feedBox">
+            <h3>Name of uploader</h3>
+        <p>Caption </p>
+        <img src=""/>
+            </div>
+        </div>
 
         )
     }
