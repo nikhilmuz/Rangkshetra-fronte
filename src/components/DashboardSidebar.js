04b81962f978c2b26b23c3a44c72abe71124b2b6
Nikhil Kumar <nikhil.nikhil.muz@gmail.com>
2018-10-04 03:17:21 +0530

    Signup and Logout API call

--- src/components/DashboardSidebar.js
+++ src/components/DashboardSidebar.js
@@ -1,6 +1,39 @@
 import React,{Component} from 'react'
 import './../css/dashboard-sidebar.css'
+import axios from "axios";
+import {API_ROOT} from "../Config";
+
+const LOGOUT_API = 'users/logout';
 export default class DashboardSidebar extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.logout = this.logout.bind(this);
+    }
+    logout(){
+        axios
+            .get(
+                API_ROOT+LOGOUT_API,
+                {
+                    headers: {
+                        Authorization: 'Token ' + localStorage.getItem('Token')
+                    }
+                },
+            )
+            .then(
+                response =>
+                {
+                    localStorage.removeItem('Token');
+                    window.location.href='/'
+                }
+            )
+            .catch(
+                error =>
+                {
+                    alert(error);
+                }
+            )
+    }
     render(){
         return(
             <div>
@@ -15,7 +48,7 @@ export default class DashboardSidebar extends Component{
                         <li className = "active"> <a href = "dashboard"> <i className = "fa fa-home"> </i>Home</a> </li>
 
                         <li> <a href ='upload'> <i className = "fa fa-upload"/>Upload</a></li>
-                        <li><a><i className = "fa fa-sign-out"/>Log Out</a></li>
+                        <li onClick={this.logout}><a><i className = "fa fa-sign-out"/>Log Out</a></li>
                     </ul>
                 </div>
             </div>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -5,84 +5,91 @@ import Footer from '../components/Footer'
 import axios from "axios";
 import {API_ROOT} from "../Config";
 
-const SIGNUP_API = 'users/signup/';
+const SIGNUP_API = 'users/register/';
 
 export default class Signup extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            name: '',
+            fname: '',
+            lname: '',
             email: '',
-            password: '',
-            confirmPassword: '',
+            pwd: '',
+            confPwd: '',
         };
-        this.handlenameChange = this.handlenameChange.bind(this);
-        this.handleemailChange = this.handleemailChange.bind(this);
-        this.handlepasswordChange = this.handlepasswordChange.bind(this)
+        this.handleFnameChange = this.handleFnameChange.bind(this);
+        this.handleLnameChange = this.handleLnameChange.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePwdChange = this.handlePwdChange.bind(this);
+        this.handleConfPwdChange = this.handleConfPwdChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-    handlenameChange(e){
-        this.setState({name: e.target.value });
+    handleFnameChange(e){
+        this.setState({fname: e.target.value });
     }
-    handleemailChange(e){
+    handleLnameChange(e){
+        this.setState({lname: e.target.value });
+    }
+    handleEmailChange(e){
         this.setState({email: e.target.value });
     }
-    handlepasswordChange(e){
-        this.setState({password: e.target.value });
+    handlePwdChange(e){
+        this.setState({pwd: e.target.value });
+    }
+    handleConfPwdChange(e){
+        this.setState({confPwd: e.target.value });
     }
 
-
-
-    handleSubmit() {
-        {
-            var payload = {
-                name: this.state.Name,
-                email: this.state.email,
-                password: this.state.password,
-
-            };
-
-
-            console.log(payload);
+    handleSubmit(){
+        if(this.state.pwd==this.state.confPwd){
             axios
-                .post(API_ROOT + SIGNUP_API, payload)
-                .then((response) => {
-                    console.log(response);
-                    window.location.href = '/user/dashboard';
-
-                })
-                .catch(error => {alert(error)})
+                .post(
+                    API_ROOT+SIGNUP_API,
+                    {
+                        email: this.state.email,
+                        first_name: this.state.fname,
+                        last_name: this.state.lname,
+                        password: this.state.pwd,
+                    }
+                )
+                .then(
+                    response =>
+                    {
+                        console.log(response);
+                        window.location.href = '/user/dashboard';
+                        localStorage.setItem('Token', response.data.token );
+                    }
+                )
+                .catch(
+                    error =>
+                    {
+                        alert(error);
+                    }
+                )
         }
-
+        else {
+            alert('Passwords doesn\'t match');
+        }
     }
 
     render(){
         return(
             <div>
                 <NavBar/>
-            <div className= "signup-box">
-                <h1> Sign up Rangkshetra </h1>
-            <div className = "right">
+                <div className= "signup-box">
+                    <h1> Sign up Rangkshetra </h1>
+                    <div className = "right">
 
-        <label>Name</label>
-        <input type = "text" name = "Full Name" value={this.state.Name} placeholder = "Full Name" required/> <br/>
+                        <input type = "text" onChange={this.handleFnameChange} placeholder = "First Name" required />
+                        <input type = "text" onChange={this.handleLnameChange} placeholder = "Last Name"/> <br/>
+                        <input type="text" onChange={this.handleEmailChange} placeholder="E-mail" required /> < br/>
+                        <input type="password" onChange={this.handlePwdChange} placeholder="Password" required /> < br/>
+                        <input type="password" onChange={this.handleConfPwdChange} placeholder="Retype password" required/> < br/>
+                        <input type = "submit" onClick={this.handleSubmit} value = "Sign me up"/>
 
-        <label> Email - Id </label>
-        <input type="text" name="email" onChange={this.handleemailChange} placeholder="E-mail"/> < br/>
-
-        <label> Password </label>
-        <input type="password" name="password" value={this.state.password}
-               onChange={this.handlepasswordChange}
-                placeholder="Password" required/> < br/>
-
-        <label> Confirm Password </label>
-        <input type="password" name="password2" placeholder="Retype password" required/> < br/>
-
-        <input type = "submit" onClick={this.handleSubmit} name = "signup_submit" value = "Sign me up"/>
-
-                <p align="center">or <a href='/login'>login</a> instead.</p>
-            </div>
-            </div>
+                        <p align="center">or <a href='/login'>login</a> instead.</p>
+                    </div>
+                </div>
 
                 {/* <div className="left">
             <span className="loginwith">Sign in with<br/>social network</span>
@@ -91,8 +98,8 @@ export default class Signup extends Component{
             <button className="social-signin google">Log in with Gmail</button>
         </div>
         < div className = "or" > OR </div>*/}
-                    <Footer/>
-    </div>
+                <Footer/>
+            </div>
         )
     }
 }
