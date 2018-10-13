4b3ff6ab55ac48773433e48b92c39c3180127ac0
Nikhil Kumar <nikhil.nikhil.muz@gmail.com>
2018-10-13 07:05:06 +0530

    Landing Page Product Switch

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,10 +2,40 @@ import React, {Component} from 'react'
 import ReactFullpage from '@fullpage/react-fullpage';
 import 'fullpage.js/vendors/scrolloverflow'
 
+function Product(props){
+    console.log(props);
+    switch(props.product){
+        case 1:
+            return <div>First</div>;
+        case 2:
+            return <div>Second</div>;
+        case 3:
+            return <div>Third</div>;
+    }
+}
+
 export default class Landing extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            active_product: 1,
+        };
+        this.selectFirst = this.selectFirst.bind(this);
+        this.selectSecond = this.selectSecond.bind(this);
+        this.selectThird = this.selectThird.bind(this);
+    }
     onLeave(origin, destination, direction) {
         // arguments are mapped in order of fullpage.js callback arguments
         // do something with the event
+    }
+    selectFirst(){
+        this.setState({active_product : 1})
+    }
+    selectSecond(){
+        this.setState({active_product : 2})
+    }
+    selectThird(){
+        this.setState({active_product : 3})
     }
     render(){
         return(
@@ -20,7 +50,10 @@ export default class Landing extends Component{
                                 <h3>Section 1</h3>
                             </div>
                             <div className="section">
-                                dtfyguhijhgfdyujuhuoil
+                                <div onClick={this.selectFirst}>Select First</div>
+                                <div onClick={this.selectSecond}>Select Second</div>
+                                <div onClick={this.selectThird}>Select Third</div>
+                                <Product product={this.state.active_product}/>
                             </div>
                         </ReactFullpage.Wrapper>
                     );
