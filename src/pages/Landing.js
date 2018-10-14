7dcdfbddd9a85fb056abc6cc8b133d97c4b24549
Nikhil Kumar <nikhil.nikhil.muz@gmail.com>
2018-10-14 06:09:37 +0530

    Material UI Tabs

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react'
 import ReactFullpage from '@fullpage/react-fullpage';
 import 'fullpage.js/vendors/scrolloverflow'
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import AppBar from "@material-ui/core/AppBar";
 
+/**
+ * @return {null}
+ */
 function Product(props){
     switch(props.product){
-        case 1:
+        case 0:
             return <div>First</div>;
-        case 2:
+        case 1:
             return <div>Second</div>;
-        case 3:
+        case 2:
             return <div>Third</div>;
+        default:
+            return null;
     }
 }
 
@@ -17,24 +25,16 @@ export default class Landing extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            active_product: 1,
+            active_product: 0,
         };
-        this.selectFirst = this.selectFirst.bind(this);
-        this.selectSecond = this.selectSecond.bind(this);
-        this.selectThird = this.selectThird.bind(this);
+        this.handleProductChange = this.handleProductChange.bind(this);
     }
     onLeave(origin, destination, direction) {
         // arguments are mapped in order of fullpage.js callback arguments
         // do something with the event
     }
-    selectFirst(){
-        this.setState({active_product : 1})
-    }
-    selectSecond(){
-        this.setState({active_product : 2})
-    }
-    selectThird(){
-        this.setState({active_product : 3})
+    handleProductChange(event, value){
+        this.setState({active_product : value})
     }
     render(){
         return(
@@ -49,9 +49,13 @@ export default class Landing extends Component{
                                 <h3>Section 1</h3>
                             </div>
                             <div className="section">
-                                <div onClick={this.selectFirst}>Select First</div>
-                                <div onClick={this.selectSecond}>Select Second</div>
-                                <div onClick={this.selectThird}>Select Third</div>
+                                <AppBar position="relative">
+                                    <Tabs fullWidth value={this.state.active_product} onChange={this.handleProductChange}>
+                                        <Tab label="Daily Comics" />
+                                        <Tab label="Item Two" />
+                                        <Tab label="RangKshetra"/>
+                                    </Tabs>
+                                </AppBar>
                                 <Product product={this.state.active_product}/>
                             </div>
                         </ReactFullpage.Wrapper>
