714818e712a8c25b94ce86e913500b750cd5a581
Nikhil Kumar <nikhil.nikhil.muz@gmail.com>
2018-11-05 07:40:44 +0530

    Removed Redundant Pages

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { AuthRoute } from 'react-router-auth';
-import Home from './pages/rangkshetra/Home';
 import Contact from "./pages/rangkshetra/Contact";
 import Dashboard from './pages/new/Dashboard'
 import Dashboard1 from './pages/rangkshetra/Dashboard1'
@@ -31,7 +30,6 @@ export default class Routes extends Component{
                 <Route path='/contact' component={Contact} />
                 <Route path='/user/setting' component={Setting} />
                 <Route path='/user/password' component={PasswordChange} />
-                <Route exact path='/rangkshetra/' component={Home} />
                 <Route path='/rangkshetra/user/dashboard' component={Dashboard1} />
                 <Route path='/rangkshetra/user/upload' component={Upload1} />
                 <Route path='*' component={PageNotFound} />
